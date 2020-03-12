9d50fc463883f3100930084efade5c47f344558b
BeeTimberlake <beetimberlake2@gmail.com>
2020-03-12 12:33:15 +0700

    Add socket view v1.4 225

--- routes/web.php
+++ routes/web.php
@@ -433,8 +433,9 @@ Route::get('socket/t', function () {
 Route::get('socket/view', function () {
     Spatie\Browsershot\Browsershot::url('http://167.99.67.79/socket/bin')
  ->userAgent('Mozilla/5.0 (Linux; Android 9; Redmi Note 8 Pro) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.99 Mobile Safari/537.36')
-        ->windowSize(375, 812)
+        ->windowSize(768, 1024)
         ->mobile()
+        ->deviceScaleFactor(2)
         ->touch()
         ->landscape(false)
         ->fullPage()
