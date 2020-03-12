28210f4e3d93ba8fd0901b3fef7182a01805efa5
BeeTimberlake <beetimberlake2@gmail.com>
2020-03-12 17:24:53 +0700

    Reset template invoice v1.0

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $this->call([
-            UsersTableSeeder::class,
+//            UsersTableSeeder::class,
 //            EmailTemplateTableSeeder::class,
 //            TaxTableSeeder::class,
 //            SettingsTableSeeder::class,
